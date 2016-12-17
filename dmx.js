fb4ad11d7e52a97d41577e5696917456ceeaeded
Michael Träger <git@m.traeger-mail.de>
2016-12-17 23:35:34 +0100

    blackout to dmx mgmt file - more universal

--- dmx.js
+++ dmx.js
@@ -8,10 +8,12 @@ function DMX(options) {
 	this.universes = {}
 	this.drivers   = {}
 	this.devices   = options.devices || require('./devices')
+    this.blackout  = false;
+	this.blackoutBuffer = {};
 
 	this.registerDriver('null',                require('./drivers/null'))
-	//this.registerDriver('enttec-usb-dmx-pro',  require('./drivers/enttec-usb-dmx-pro'))
-	//this.registerDriver('enttec-open-usb-dmx', require('./drivers/enttec-open-usb-dmx'))
+	this.registerDriver('enttec-usb-dmx-pro',  require('./drivers/enttec-usb-dmx-pro'))
+	this.registerDriver('enttec-open-usb-dmx', require('./drivers/enttec-open-usb-dmx'))
 	this.registerDriver('art-net',             require('./drivers/art-net'))
 }
 
@@ -30,18 +32,40 @@ DMX.prototype.addUniverse = function(name, driver, device_id) {
 }
 
 DMX.prototype.update = function(universe, channels) {
-	this.universes[universe].update(channels)
+	if (this.blackout == false) { //send "data" only if no blackout
+		this.universes[universe].update(channels)
+	}else { //update buffer on changes while blackout
+        for (var channel in channels) {
+            this.blackoutBuffer[channel] = channels[channel];
+        }
+	}
 	this.emit('update', universe, channels)
 }
 
 DMX.prototype.updateAll = function(universe, value) {
-  this.universes[universe].updateAll(value)
-  this.emit('updateAll', universe, value)
+    if (this.blackout == false) { //send "data" only if no blackout
+        this.universes[universe].updateAll(value)
+	} else { //update buffer on changes while blackout
+        for (var i = 0; i < 512; i++) {
+            this.blackoutBuffer[i] = value;
+        }
+	}
+	this.emit('updateAll', universe, value)
 }
 
 DMX.prototype.toggleBlackout = function(universe) {
-	var blackout = this.universes[universe].toggleBlackout();
-	this.emit('blackout', blackout);
+	if (this.blackout == false) {
+		this.blackout = true;
+		for (var i = 0; i < 512; i++) { //store current state
+            this.blackoutBuffer[i] = this.universes[universe].get(i);
+        }
+        this.universes[universe].updateAll(0)
+    } else {
+        this.universes[universe].update(this.blackoutBuffer); //restore values
+        this.blackoutBuffer = {};
+		this.blackout = false;
+	}
+    this.emit('blackout', this.blackout);
 }
 
 module.exports = DMX
